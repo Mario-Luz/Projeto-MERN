63b368ace8a9de536f53466ac0cf3488f51ba3b5
Mario Luz <marionetoluz@gmail.com>
2022-09-29 12:59:39 -0300

    adicionando CRUD produtos

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = express.Router();
 const Usuario = require('./controllers/usuario.controllers');
-
+const Produto = require('./controllers/produtos.controller');
 
 routes.get('/api',Usuario.index );
 //Rotas de Usuarios
@@ -10,6 +10,13 @@ routes.get('/api/usuarios',Usuario.index );
 routes.get('/api/usuarios.details/:_id',Usuario.details);
 routes.delete('/api/usuarios.delete/:_id',Usuario.delete);
 routes.put('/api/usuarios',Usuario.update);
+
+//Rotas de Produto
+routes.post('/api/produtos', Produto.create);
+routes.get('/api/produtos',Produto.index );
+routes.get('/api/produtos/:_id',Produto.details);
+routes.delete('/api/produtos/:_id',Produto.delete);
+routes.put('/api/produtos',Produto.update);
 
 
 
